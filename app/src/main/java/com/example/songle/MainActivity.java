b3307409c364ce536dfa9773238f48c927ec4e86
siryessir1 <lapilosew2003@gmail.com>
2017-10-24 15:02:15 +0100

    Obvious errors fixed

--- app/src/main/java/com/example/songle/MainActivity.java
+++ app/src/main/java/com/example/songle/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.songle;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
@@ -38,40 +40,50 @@ public class MainActivity extends FragmentActivity implements DownloadCallback {
     //Broadcast receiver that tracks network connectivity changes
     private NetworkReceiver receiver = new NetworkReceiver();
 
-    // Keep a reference to the NetworkFragment , which owns the AsyncTask object that is
-    //used to execute network ops.
+    // Keep a reference to the NetworkFragment which owns the AsyncTask object
+    // that is used to execute network ops.
     private NetworkFragment mNetworkFragment;
+
+
+
 
     // boolean telling us whether a download is in progress so we don't trigger overlapping
     // downloads with consecutive button clicks
     private boolean mDownloading = false;
 
-    Context context = getActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //check for internet access first
+        boolean networkOn = isNetworkAvailable(this);
+        //if there is internet, load as normal
+        if (!networkOn) {
+            //with no internet, send an alert that will close the app.
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("A network connection is required! Please check your connection and load again");
+            alertDialogBuilder.setPositiveButton(R.string.okay,
+                    new DialogInterface.OnClickListener(){
+                        @Override
+                        public void onClick(DialogInterface arg0, int arg1){
+                            System.exit(0);
+                        }
+                    });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+
+
         // Register BroadcastReceiver to track connection changes
         IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
         receiver = new NetworkReceiver();
         this.registerReceiver(receiver, filter);
 
 
-        mNetworkFragment = NetworkFragment.getInstance(getFragmentManager(), "https://www.google.com");
-
-        //check for internet access first (need for saved data)
-        boolean networkOn = isNetworkAvailable(this);
-        //if there is internet, load as normal
-        if (!networkOn) {
-            //with no internet, send an alert that will close the app.
-            NoInternetAccessDialogFragment networkError = new NoInternetAccessDialogFragment();
-            //TODO doesn't work - just goes blank
-            networkError.getDialog();
-            System.exit(0);
-        }
-    //TODO check this is the correct way to find the string value
-        SharedPreferences save = getSharedPreferences(String.valueOf(R.string.saved_timestamp), 0);
+        mNetworkFragment = NetworkFragment.getInstance(getSupportFragmentManager(),
+                getResources().getString(R.string.url_songs_xml));
 
 
     }
@@ -121,7 +133,7 @@ public class MainActivity extends FragmentActivity implements DownloadCallback {
     private void startDownload(){
         if (!mDownloading && mNetworkFragment != null){
             //Execute the async download.
-            mNetworkFragment.startDownload();
+            mNetworkFragment.startGeneralDownload();
             mDownloading = true;
         }
     }
@@ -136,13 +148,31 @@ public class MainActivity extends FragmentActivity implements DownloadCallback {
     }
 
     @Override
-    public NetworkInfo getActiveNetworkInfo(){
-
+    public NetworkInfo getActiveNetworkInfo() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo;
     }
+
 
     @Override
     public void onProgressUpdate(int progressCode, int percentComplete) {
-
+        switch(progressCode) {
+            // You can add UI behavior for progress updates here.
+            case Progress.ERROR:
+                break;
+            case Progress.CONNECT_SUCCESS:
+                break;
+            case Progress.GET_INPUT_STREAM_SUCCESS:
+                break;
+            case Progress.PROCESS_INPUT_STREAM_IN_PROGRESS:
+                //could add something like this
+                //mDataText.setText("" + percentComplete + "%");
+                break;
+            case Progress.PROCESS_INPUT_STREAM_SUCCESS:
+                break;
+        }
     }
 
     @Override
@@ -150,82 +180,6 @@ public class MainActivity extends FragmentActivity implements DownloadCallback {
 
     }
 
-    public Context getActivity() {
-        return this;
-    }
-
-    private class DownloadXmlTask extends AsyncTask<String, Void, String> {
-        private String TAG = DownloadXmlTask.class.getSimpleName();
-        private String mostRecentXML;
-
-        @Override
-        protected  String doInBackground(String... urls){
-            Log.v(TAG, "Started loading XML in the background");
-            try {
-                return loadXmlFromNetwork(urls[0]);
-            } catch (IOException e){
-                return "Unable to load content. Check your network connection";
-            } catch (XmlPullParserException e){
-                return "Error parsing XML";
-            }
-        }
-
-        //Given a string representation of a URL, sets up a connection and gets
-        //an input stream
-        private InputStream downloadUrl(String urlString) throws IOException {
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            conn.setReadTimeout(10000 /*milliseconds*/);
-            conn.setConnectTimeout(15000 /*milliseconds*/);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true);
-            Log.v(TAG, "Preparing to download");
-            conn.connect();
-            return conn.getInputStream();
-
-        }
 
 
-        private String loadXmlFromNetwork(String urlString) throws
-                XmlPullParserException, IOException{
-            StringBuilder result = new StringBuilder();
-            InputStream stream = null;
-            //Instantiate the parser.
-            XmlParser parser = new XmlParser();
-            List<XmlParser.Song> songs = null;
-            String title = null;
-            String url = null;
-            String summary = null;
-            Calendar rightNow = Calendar.getInstance();
-            DateFormat formatter = new SimpleDateFormat("MMM dd h:mmaa");
-
-
-            try {
-                stream = downloadUrl(urlString);
-                songs = parser.parse(stream);
-
-                String timestamp = parser.getXmlTimestamp(stream);
-                if (timestamp.equals(mostRecentXML)){
-                    return "XML up to date";
-                } else {
-                    List<XmlParser.Song> songs = parser.parse(stream);
-                    mostRecentXML = timestamp;
-                }
-            }
-            return result.toString();
-        }
-
-
-
-        @Override
-        protected void onPostExecute(String result){
-            setContentView(R.layout.activity_main);
-            //Displays the HTML string in the UI via a WebView
-            WebView myWebView = (WebView) findViewById(R.id.webview);
-            myWebView.loadData(result, "text/html", null);
-        }
-
-
-    }
 }
--- app/src/main/java/com/example/songle/NetworkFragment.java
+++ app/src/main/java/com/example/songle/NetworkFragment.java
@@ -1,22 +1,49 @@
 package com.example.songle;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
 
+import javax.net.ssl.HttpsURLConnection;
 
 /**
- * Created by Paul Moore on 19-Oct-17.
+ * Implementation of headless Fragment that runs an AsyncTask to fetch data from the network.
  */
-
 public class NetworkFragment extends Fragment {
     public static final String TAG = "NetworkFragment";
 
@@ -24,99 +51,120 @@ public class NetworkFragment extends Fragment {
 
     private DownloadCallback mCallback;
     private DownloadTask mDownloadTask;
+    private DownloadXmlTask mDownloadXmlTask;
     private String mUrlString;
+
     /**
      * Static initializer for NetworkFragment that sets the URL of the host it will be downloading
      * from.
      */
-    public static NetworkFragment getInstance(FragmentManager fragmentManager, String url){
-        NetworkFragment networkFragment = new NetworkFragment();
-        Bundle args = new Bundle();
-        args.putString(URL_KEY, url);
-        networkFragment.setArguments(args);
-        fragmentManager.beginTransaction().add(networkFragment, TAG).commit();
+    public static NetworkFragment getInstance(FragmentManager fragmentManager, String url) {
+        // Recover NetworkFragment in case we are re-creating the Activity due to a config change.
+        // This is necessary because NetworkFragment might have a task that began running before
+        // the config change and has not finished yet.
+        // The NetworkFragment is recoverable via this method because it calls
+        // setRetainInstance(true) upon creation.
+        NetworkFragment networkFragment = (NetworkFragment) fragmentManager
+                .findFragmentByTag(NetworkFragment.TAG);
+        if (networkFragment == null) {
+            networkFragment = new NetworkFragment();
+            Bundle args = new Bundle();
+            args.putString(URL_KEY, url);
+            networkFragment.setArguments(args);
+            fragmentManager.beginTransaction().add(networkFragment, TAG).commit();
+        }
         return networkFragment;
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState){
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Retain this Fragment across configuration changes in the host Activity.
+        setRetainInstance(true);
         mUrlString = getArguments().getString(URL_KEY);
-        //may include more
     }
 
     @Override
-    public void onAttach(Context context){
+    public void onAttach(Context context) {
         super.onAttach(context);
-        // host Activity will handle callbacks from a task.
-        mCallback = (DownloadCallback) context;
+        // Host Activity will handle callbacks from task.
+        mCallback = (DownloadCallback)context;
     }
 
     @Override
-    public void onDetach(){
+    public void onDetach() {
         super.onDetach();
-        // Clear reference to host Activity to avoid memory leak.
+        // Clear reference to host Activity.
         mCallback = null;
     }
 
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
         // Cancel task when Fragment is destroyed.
         cancelDownload();
         super.onDestroy();
     }
 
     /**
-     * Start non-blocking execution of DownloadTask
+     * Start non-blocking execution of DownloadTask.
      */
-    public void startDownload(){
+    public void startGeneralDownload() {
         cancelDownload();
         mDownloadTask = new DownloadTask();
         mDownloadTask.execute(mUrlString);
     }
 
     /**
-     * Cancel (and interrupt if necessary any ongoing DownloadTask execution
+     * Start non-blocking execution of DownloadXmlTask.
      */
-    public void cancelDownload(){
-        if (mDownloadTask != null){
+    public void startXmlDownload() {
+        cancelDownload();
+        mDownloadXmlTask = new DownloadXmlTask();
+        mDownloadXmlTask.execute(mUrlString);
+    }
+
+    /**
+     * Cancel (and interrupt if necessary) any ongoing DownloadTask execution.
+     */
+    public void cancelDownload() {
+        if (mDownloadTask != null) {
             mDownloadTask.cancel(true);
+            mDownloadTask = null;
         }
     }
-    private class DownloadTask extends AsyncTask<String, Void, com.example.songle.DownloadTask.Result> {
-        private DownloadCallback<String> mCallback;
 
-        DownloadTask(DownloadCallback<String> callback){
-            setCallback(callback);
-        }
-        void setCallback(DownloadCallback<String> callback){
-            mCallback = callback;
-        }
+    /**
+     * Implementation of AsyncTask that runs a network operation on a background thread.
+     */
+    private class DownloadTask extends AsyncTask<String, Integer, DownloadTask.Result> {
+
         /**
-         * Wrapper class that serves as a union of a result value and an exception. When the download
-         * task has completed, either the result value or exception can be a non-null value.
-         * This allows you to pass exceptions to the UI thread that were thrown during doInBackground().
+         * Wrapper class that serves as a union of a result value and an exception. When the
+         * download task has completed, either the result value or exception can be a non-null
+         * value. This allows you to pass exceptions to the UI thread that were thrown during
+         * doInBackground().
          */
-        static class Result {
+        class Result {
             public String mResultValue;
             public Exception mException;
-            public Result(String resultValue){
+            public Result(String resultValue) {
                 mResultValue = resultValue;
             }
-            public Result(Exception exception){
+            public Result(Exception exception) {
                 mException = exception;
             }
         }
+
         /**
          * Cancel background network operation if we do not have network connectivity.
          */
         @Override
-        protected void onPreExecute(){
-            if (mCallback != null){
+        protected void onPreExecute() {
+            if (mCallback != null) {
                 NetworkInfo networkInfo = mCallback.getActiveNetworkInfo();
                 if (networkInfo == null || !networkInfo.isConnected() ||
                         (networkInfo.getType() != ConnectivityManager.TYPE_WIFI
-                                && networkInfo.getType() != ConnectivityManager.TYPE_MOBILE)){
+                                && networkInfo.getType() != ConnectivityManager.TYPE_MOBILE)) {
                     // If no connectivity, cancel task and update Callback with null data.
                     mCallback.updateFromDownload(null);
                     cancel(true);
@@ -124,30 +172,45 @@ public class NetworkFragment extends Fragment {
             }
         }
 
+        /**
+         * Defines work to perform on the background thread.
+         */
         @Override
-        protected com.example.songle.DownloadTask.Result doInBackground(String... urls){
-            com.example.songle.DownloadTask.Result result = null;
-            if (!isCancelled() && urls != null && urls.length > 0){
+        protected Result doInBackground(String... urls) {
+            Result result = null;
+            if (!isCancelled() && urls != null && urls.length > 0) {
                 String urlString = urls[0];
                 try {
                     URL url = new URL(urlString);
                     String resultString = downloadUrl(url);
-                    if (resultString != null){
-                        result = new com.example.songle.DownloadTask.Result(resultString);
+                    if (resultString != null) {
+                        result = new Result(resultString);
                     } else {
                         throw new IOException("No response received.");
                     }
-                } catch(Exception e){
-                    result = new com.example.songle.DownloadTask.Result(e);
+                } catch(Exception e) {
+                    result = new Result(e);
                 }
             }
             return result;
         }
+
+        /**
+         * Send DownloadCallback a progress update.
+         */
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            if (values.length >= 2) {
+                mCallback.onProgressUpdate(values[0], values[1]);
+            }
+        }
+
         /**
          * Updates the DownloadCallback with the result.
          */
         @Override
-        protected void onPostExecute(com.example.songle.DownloadTask.Result result) {
+        protected void onPostExecute(Result result) {
             if (result != null && mCallback != null) {
                 if (result.mException != null) {
                     mCallback.updateFromDownload(result.mException.getMessage());
@@ -162,7 +225,173 @@ public class NetworkFragment extends Fragment {
          * Override to add special behavior for cancelled AsyncTask.
          */
         @Override
-        protected void onCancelled(com.example.songle.DownloadTask.Result result) {
+        protected void onCancelled(Result result) {
         }
+
+        /**
+         * Given a URL, sets up a connection and gets the HTTP response body from the server.
+         * If the network request is successful, it returns the response body in String form. Otherwise,
+         * it will throw an IOException.
+         */
+        private String downloadUrl(URL url) throws IOException {
+            InputStream stream = null;
+            HttpsURLConnection connection = null;
+            String result = null;
+            try {
+                connection = (HttpsURLConnection) url.openConnection();
+                // Timeout for reading InputStream arbitrarily set to 3000ms.
+                connection.setReadTimeout(3000);
+                // Timeout for connection.connect() arbitrarily set to 3000ms.
+                connection.setConnectTimeout(3000);
+                // For this use case, set HTTP method to GET.
+                connection.setRequestMethod("GET");
+                // Already true by default but setting just in case; needs to be true since this request
+                // is carrying an input (response) body.
+                connection.setDoInput(true);
+                // Open communications link (network traffic occurs here).
+                connection.connect();
+                publishProgress(DownloadCallback.Progress.CONNECT_SUCCESS);
+                int responseCode = connection.getResponseCode();
+                if (responseCode != HttpsURLConnection.HTTP_OK) {
+                    throw new IOException("HTTP error code: " + responseCode);
+                }
+                // Retrieve the response body as an InputStream.
+                stream = connection.getInputStream();
+                publishProgress(DownloadCallback.Progress.GET_INPUT_STREAM_SUCCESS, 0);
+                if (stream != null) {
+                    // Converts Stream to String with max length of 500.
+                    result = readStream(stream, 500);
+                    publishProgress(DownloadCallback.Progress.PROCESS_INPUT_STREAM_SUCCESS, 0);
+                }
+            } finally {
+                // Close Stream and disconnect HTTPS connection.
+                if (stream != null) {
+                    stream.close();
+                }
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+            return result;
+        }
+
+        /**
+         * Converts the contents of an InputStream to a String.
+         */
+        private String readStream(InputStream stream, int maxLength) throws IOException {
+            String result = null;
+            // Read InputStream using the UTF-8 charset.
+            InputStreamReader reader = new InputStreamReader(stream, "UTF-8");
+            // Create temporary buffer to hold Stream data with specified max length.
+            char[] buffer = new char[maxLength];
+            // Populate temporary buffer with Stream data.
+            int numChars = 0;
+            int readSize = 0;
+            while (numChars < maxLength && readSize != -1) {
+                numChars += readSize;
+                int pct = (100 * numChars) / maxLength;
+                publishProgress(DownloadCallback.Progress.PROCESS_INPUT_STREAM_IN_PROGRESS, pct);
+                readSize = reader.read(buffer, numChars, buffer.length - numChars);
+            }
+            if (numChars != -1) {
+                // The stream was not empty.
+                // Create String that is actual length of response body if actual length was less than
+                // max length.
+                numChars = Math.min(numChars, maxLength);
+                result = new String(buffer, 0, numChars);
+            }
+            return result;
+        }
+    }
+
+    private class DownloadXmlTask extends AsyncTask<String, Void, String> {
+        private String TAG = DownloadXmlTask.class.getSimpleName();
+        private String mostRecentXML = getResources().getString(R.string.saved_timestamp);
+
+        /**
+         * Cancel background network operation if we do not have network connectivity.
+         */
+        @Override
+        protected void onPreExecute() {
+            if (mCallback != null) {
+                NetworkInfo networkInfo = mCallback.getActiveNetworkInfo();
+                if (networkInfo == null || !networkInfo.isConnected() ||
+                        (networkInfo.getType() != ConnectivityManager.TYPE_WIFI
+                                && networkInfo.getType() != ConnectivityManager.TYPE_MOBILE)) {
+                    // If no connectivity, cancel task and update Callback with null data.
+                    mCallback.updateFromDownload(null);
+                    cancel(true);
+                }
+            }
+        }
+
+        @Override
+        protected  String doInBackground(String... urls){
+            Log.v(TAG, "Started loading XML in the background");
+            try {
+                return loadXmlFromNetwork(urls[0]);
+            } catch (IOException e){
+                return "Unable to load content. Check your network connection";
+            } catch (XmlPullParserException e){
+                return "Error parsing XML";
+            }
+        }
+
+        private String loadXmlFromNetwork(String urlString) throws
+                XmlPullParserException, IOException{
+            StringBuilder result = new StringBuilder();
+            InputStream stream = null;
+            //Instantiate the parser.
+            XmlParser parser = new XmlParser();
+            List<XmlParser.Song> songs = null;
+            String title = null;
+            String url = null;
+            String summary = null;
+            Calendar rightNow = Calendar.getInstance();
+            DateFormat formatter = new SimpleDateFormat("MMM dd h:mmaa");
+
+
+            try {
+                stream = downloadUrl(urlString);
+                songs = parser.parse(stream);
+
+                String timestamp = parser.getXmlTimestamp(stream);
+                if (timestamp.equals(mostRecentXML)){
+                    return "XML up to date";
+                } else {
+
+                    mostRecentXML = timestamp;
+                }
+            } catch(Exception e){
+
+            }
+            return result.toString();
+        }
+
+        //Given a string representation of a URL, sets up a connection and gets
+        //an input stream
+        private InputStream downloadUrl(String urlString) throws IOException {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setReadTimeout(10000 /*milliseconds*/);
+            conn.setConnectTimeout(15000 /*milliseconds*/);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            Log.v(TAG, "Preparing to download");
+            conn.connect();
+            return conn.getInputStream();
+
+        }
+
+
+
+        @Override
+        protected void onPostExecute(String result){
+
+
+        }
+
+
     }
 }
